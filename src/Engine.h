f9475c4210987fcbe6de4ca04baee21c5a762acb
SturkaCircutz <313721325sjw@gmail.com>
2026-04-02 17:08:40 -0600

    Add player collision and grounded movement

--- src/Engine.h
+++ src/Engine.h
@@ -14,12 +14,17 @@ public:
 
 private:
     void ProcessInput(float deltaTime);
+    void MovePlayer(const Vec3& velocityStep);
+    bool IsPlayerCollidingAt(const Vec3& cameraPosition) const;
     void RenderFrame(int width, int height);
     void ApplyPerspective(float aspectRatio, float fieldOfView, float nearPlane, float farPlane) const;
 
     GLFWwindow* window = nullptr;
     Camera camera;
     World world;
+    Vec3 playerVelocity;
+    bool isGrounded = false;
+    bool jumpPressedLastFrame = false;
     double lastMouseX = 0.0;
     double lastMouseY = 0.0;
     bool firstMouseSample = true;
